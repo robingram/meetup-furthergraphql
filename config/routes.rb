9cf90984297946b8417ec3e846aad39b72d4ff2b
Rob Ingram <rob@headcrash.org>
2021-04-01 12:12:19 +1300

    Remove graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
   post "/graphql", to: "graphql#execute"
   resources :comments
   resources :posts
