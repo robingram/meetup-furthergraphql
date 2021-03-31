7fc5b13d385e085dca3f671d61ed088b7538856a
Rob Ingram <rob@headcrash.org>
2021-04-01 12:43:38 +1300

    Add Post GraphQL type

---
+++ app/graphql/types/posts/post.rb
@@ -0,0 +1,15 @@
+module Types
+  module Posts
+    class Post < GraphQL::Schema::Object
+      graphql_name "Post"
+      description "A blog article"
+
+      field :id, ID, null: false
+      field :title, String, null: true
+      field :content, String, null: true
+      field :publication_date, GraphQL::Types::ISO8601Date, null: true
+      field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+      field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    end
+  end
+end
